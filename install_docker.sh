fc4c4d6de622d568033036ea66660e3f733e7870
wachid-sst <wachid-sst@users.noreply.github.com>
2023-12-19 07:12:12 +0700

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# update repo
+sudo apt update -y && sudo apt upgrade -y
+# install depedensi
+sudo apt-get install ca-certificates curl gnupg
+# Mendownload gpg key docker:
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+#Menambahkan repository kedalam system operasi
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Melakukan update package:
+sudo apt update && apt upgrade
+# Melakukan instalasi package docker
+sudo apt install install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Check informasi versi docker
+sudo docker --version
+# Check informasi status service docker
+sudo systemctl status docker
+Untuk menjalankan service docker, jalankan perintah berikut
+sudo systemctl start docker
+# Mensetting aplikasi docker berjalan di start up
+sudo systemctl enable docker
